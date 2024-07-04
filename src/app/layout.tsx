10efc08ac00a97954f8add6c3d2bc1f319850db7
kim3527 <kim3527@nate.com>
2024-07-04 14:34:09 +0900

    배경색 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from "next";
-
 import "./globals.css";
 import { Footer, Navbar } from "@/components";
-
-
 
 export const metadata: Metadata = {
   title: "Car Hub",
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <main className="overflow-hidden">
       <Hero />
    
-      <div className="bg-orange-300 mt-12 padding-x padding-y max-width" id="discover">
+      <div className=" mt-12 padding-x padding-y max-width" id="discover">
         <div className="home__text-container" >
           <h1 className="text-4xl font-extrabold">Car Catalog</h1>
           <p>Explore ther cars you might like</p>
